ff941e8da831df44016a3520cbd8f96a3b9c0a4a
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-10-28 18:29:35 +0200

    adding some functions

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const rulesBtn = document.querySelector(".rules");
 const rulesContainer = document.querySelector(".rules-container");
 const closeBtn = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
+const chooseContainer = document.querySelector(".bottom-side");
+const choose = document.querySelectorAll(".bottom-side div");
+const fightBox = document.querySelector(".fight-box");
+const playAgain = document.querySelector(".play-again");
+const count = document.querySelector(".count");
+
+let timer;
 
 const showRulesContainer = function () {
   if (
@@ -16,5 +23,33 @@ const showRulesContainer = function () {
   }
 };
 
+const showFight = function () {
+  let counter = 3;
+  count.innerText = counter;
+
+  timer = setInterval(() => {
+    counter--;
+    if (counter >= 1) {
+      count.innerText = counter;
+    } else {
+      count.innerText = "";
+      clearInterval(timer);
+    }
+  }, 1000);
+
+  setTimeout(() => {
+    chooseContainer.classList.add("none");
+    fightBox.classList.remove("none");
+  }, 3000);
+};
+
+const tryAgainFunc = function () {
+  chooseContainer.classList.remove("none");
+  fightBox.classList.add("none");
+};
+
 rulesBtn.addEventListener("click", showRulesContainer);
 closeBtn.addEventListener("click", showRulesContainer);
+overlay.addEventListener("click", showRulesContainer);
+choose.forEach((el) => el.addEventListener("click", showFight));
+playAgain.addEventListener("click", tryAgainFunc);
